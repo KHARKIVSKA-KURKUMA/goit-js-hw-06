d34bdb469f9a651fc1c6a51638e6234383837d85
Kateryna Vasiuk <catherine.vasyuk11@gmail.com>
2023-02-22 23:53:36 +0200

    3rd task

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,14 @@
 const taskItemEl = document.querySelectorAll(".item");
-console.log(`Number of categories: ` + taskItemEl.length);
+console.log(`Number of categories: ${taskItemEl.length}`);
+
+const categoriesList = document.querySelector("ul#categories");
+const items = categoriesList.querySelectorAll("li.item");
+
+items.forEach((item) => {
+  const heading = item.querySelector("h2");
+  const itemsCount = item.querySelectorAll("li").length;
+
+  console.log(`Category: ${heading.textContent}. 
+  Elements: ${itemsCount}`);
+});
+/* -------------------------------------------------------------------------- */
--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,22 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ulEl = document.querySelector("#ingredients");
+
+const addIngredients = (array) =>
+  array.map((elem) => {
+    const itemEl = document.createElement("li");
+    itemEl.textContent = elem;
+    itemEl.style.fontSize = "30px";
+    itemEl.style.color = "rgb(125 22 225)";
+    return itemEl;
+  });
+
+ulEl.append(...addIngredients(ingredients));
+/* -------------------------------------------------------------------------- */
